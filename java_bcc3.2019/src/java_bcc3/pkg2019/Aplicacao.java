407d3a665b024d47fcb405e2d64906f71538a17c
jeffitequila <43590568+jeffitequila@users.noreply.github.com>
2019-02-14 16:48:35 -0200

    Add files via upload

---
+++ java_bcc3.2019/src/java_bcc3/pkg2019/Aplicacao.java
@@ -0,0 +1,20 @@
+
+package java_bcc3.pkg2019;
+
+
+public class Aplicacao {
+
+    public static void main(String[] args) {
+       String nome = "";            
+       
+       nome = "Fulano";
+       int id = 10; double peso = 20.5;
+       
+       System.out.println("O nome é \n" + nome + 
+               " E minha idade é \n" + id +  
+               " E meu peso é \n" + peso);
+       
+        
+    }
+    
+}
